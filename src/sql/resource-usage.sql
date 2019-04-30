409dc046c2529f152a8bc58e4b2b5addaefb0d33
Jovan Popovic <jocapc@gmail.com>
2019-04-30 11:07:57 +0200

    Formatted time in resource usage

--- src/sql/resource-usage.sql
+++ src/sql/resource-usage.sql
@@ -1,6 +1,6 @@
 IF( SERVERPROPERTY('EngineEdition') = 8 )
 select
-    [time] = CAST(start_time AS smalldatetime),
+    [time] = CONVERT(VARCHAR(5), CAST(start_time AS smalldatetime), 108),
     [storage usage %] = AVG(storage_space_used_mb)/AVG(reserved_storage_mb) *100,
     [cpu usage %] = AVG(avg_cpu_percent)
 from master.sys.server_resource_stats
